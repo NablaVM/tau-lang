f9578e8f2ef3526be26119b13dd2acd174198d4b
bosley <bosley117@gmail.com>
2020-07-22 19:48:27 -0400

    Added test thing

--- src/app_tests/parser/TokenStream.cpp
+++ src/app_tests/parser/TokenStream.cpp
@@ -35,7 +35,9 @@ namespace
                 "'\\n'",    // We add multiple slashes to ensure that it is as
                 "'\\r'",    // what would appear in the file '\n'
                 "'\\0'",
-                "'\\''"
+                "'\\''",
+                "'0'", "'1'", "'2'", "'3'", "'4'", "'5'","'6'","'7'","'8'","'9'",
+                "'^'", "'.'"
             };
 
             int selection = TEST::getRandom<int>(0, options.size()-1);
